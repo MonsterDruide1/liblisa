6d770440412c7b09bb0910add2bf29b43eb5ab05
MonsterDruide1 <5958456@gmail.com>
2026-07-23 19:18:41 +0200

    use libsla without debug

--- arch/ghidra-x64/liblisa-ghidra-x64-observer/build.rs
+++ arch/ghidra-x64/liblisa-ghidra-x64-observer/build.rs
@@ -14,7 +14,7 @@ fn main() {
     // Tell cargo to tell rustc to link the system bzip2
     // shared library.
     println!("cargo:rustc-link-lib=ghidraemu");
-    println!("cargo:rustc-link-lib=sla_dbg");
+    println!("cargo:rustc-link-lib=sla");
     println!("cargo:rustc-link-lib=stdc++");
     println!("cargo:rustc-link-lib=z");
 
