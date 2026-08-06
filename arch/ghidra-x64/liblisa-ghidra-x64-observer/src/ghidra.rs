193bea1ae9abed9046f189feb430d2499d025d18
MonsterDruide1 <5958456@gmail.com>
2026-08-06 21:20:22 +0200

    ghidra: support `EmulationUnimplemented` exception

--- arch/ghidra-x64/liblisa-ghidra-x64-observer/src/ghidra.rs
+++ arch/ghidra-x64/liblisa-ghidra-x64-observer/src/ghidra.rs
@@ -180,6 +180,7 @@ impl GhidraObserver {
                 bind::ExceptionType_GeneralProtectionFault => return Err(OracleError::GeneralFault),
                 bind::ExceptionType_CustomPcodeOpCalled => return Err(OracleError::ApiError("Ghidra emulator called a custom pcode op".to_string())),
                 bind::ExceptionType_VarnodeTooLarge => return Err(OracleError::ApiError("Ghidra emulator reported varnode too large".to_string())),
+                bind::ExceptionType_EmulationUnimplemented => return Err(OracleError::ApiError("Ghidra emulator reported emulation unimplemented".to_string())),
                 exception => unreachable!("Ghidra emulator threw unexpected exception: {:?}", exception),
             }
             let new_state = *observation_result.after;
@@ -202,6 +203,7 @@ impl GhidraObserver {
                 bind::ExceptionType_GeneralProtectionFault => return Err(OracleError::GeneralFault),
                 bind::ExceptionType_CustomPcodeOpCalled => return Err(OracleError::ApiError("Ghidra emulator called a custom pcode op".to_string())),
                 bind::ExceptionType_VarnodeTooLarge => return Err(OracleError::ApiError("Ghidra emulator reported varnode too large".to_string())),
+                bind::ExceptionType_EmulationUnimplemented => return Err(OracleError::ApiError("Ghidra emulator reported emulation unimplemented".to_string())),
                 exception => unreachable!("Ghidra emulator threw unexpected exception during memory access scan: {:?}", exception),
             }
             let accesses = *scan_result.accesses;
